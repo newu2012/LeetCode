8bc658fe7bbac7c79fac22e474d652e207863aec
newu2012 <newu2011@gmail.com>
2021-07-28 20:53:44 +0500

    7. Reverse Integer

---
+++ LeetCode/7. Reverse Integer.cs
@@ -0,0 +1,59 @@
+using System;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCode {
+    public class ReverseIntegerTask {
+        public int Reverse(int x) {
+            var minus = x < 0;
+
+            if (minus)
+                x *= -1;
+
+            var reversed = 0L;
+            while (x > 0) {
+                reversed = reversed * 10 + x % 10;
+                x /= 10;
+            }
+
+            return reversed > int.MaxValue ? 0 : 
+                minus ? (int) (-1 * reversed) : (int) reversed;
+        }
+    }
+}
+namespace LeetCodeTests {
+    [TestFixture]
+    public class ReverseIntegerTaskTest {
+        private ReverseIntegerTask task;
+
+        [SetUp]
+        public void SetUp() {
+            task = new ReverseIntegerTask();
+        }
+        
+        [Test]
+        public void Example1() {
+            Assert.AreEqual(task.Reverse(123), 321);
+        }
+        
+        [Test]
+        public void Example2() {
+            Assert.AreEqual(task.Reverse(-123), -321);
+        }
+        
+        [Test]
+        public void Example3() {
+            Assert.AreEqual(task.Reverse(120), 21);
+        }
+        
+        [Test]
+        public void Example4() {
+            Assert.AreEqual(task.Reverse(0), 0);
+        }
+        
+        [Test]
+        public void BigNumber() {
+            Assert.AreEqual(task.Reverse(int.MaxValue), 0);
+        }
+    }
+}
